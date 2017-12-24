e8cf5b341de19baa861421c5edca847f2bd48427
emethg <34809579+emethg@users.noreply.github.com>
2017-12-24 13:33:15 +0200

    Add files via upload

---
+++ reset_password.php
@@ -0,0 +1,34 @@
+<?php
+
+require 'db.php';
+session_start();
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') { 
+
+
+    if ( $_POST['newpassword'] == $_POST['confirmpassword'] ) { 
+
+        $new_password = password_hash($_POST['newpassword'], PASSWORD_BCRYPT);
+        
+
+        $email = $mysqli->escape_string($_POST['email']);
+        $hash = $mysqli->escape_string($_POST['hash']);
+        
+        $sql = "UPDATE users SET password='$new_password', hash='$hash' WHERE email='$email'";
+
+        if ( $mysqli->query($sql) ) {
+
+        $_SESSION['message'] = "Your password has been reset successfully!";
+        header("location: success.php");    
+
+        }
+
+    }
+    else {
+        $_SESSION['message'] = "Two passwords you entered don't match, try again!";
+        header("location: error.php");    
+    }
+
+}
+?>
