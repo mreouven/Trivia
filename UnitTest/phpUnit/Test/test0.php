3273e6547b92ac17ac041f33e12b4e262923c24d
mreouven <32807666+mreouven@users.noreply.github.com>
2018-01-06 23:38:34 +0200

    test

---
+++ UnitTest/phpUnit/Test/test0.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace phpUnitTutorial\Test;
+
+class Test extends \PHPUnit_Framework_TestCase
+{
+	public function testTrueIsTrue()
+		{
+		    $foo = true;
+		    $this->assertTrue($foo);
+		}
+    //
+}
