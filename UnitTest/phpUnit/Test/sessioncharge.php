222ee50e9bc824edd44b67954dbd796abcb81a05
mreouven <32807666+mreouven@users.noreply.github.com>
2018-01-07 11:46:16 +0200

    TEst

---
+++ UnitTest/phpUnit/Test/sessioncharge.php
@@ -0,0 +1,17 @@
+  <?php   
+include 'db.php';
+
+$email = $mysqli->escape_string($_POST['email']);
+$result = $mysqli->query("SELECT * FROM users WHERE email='$email'");
+$user = $result->fetch_assoc();
+
+ 	    $_SESSION['email'] = $user['email'];
+        $_SESSION['first_name'] = $user['first_name'];
+        $_SESSION['last_name'] = $user['last_name'];
+        $_SESSION['active'] = $user['active'];
+		$_SESSION['level_user'] = $user['level_user'];
+		$_SESSION['admin'] = $user['admin'];
+        $_SESSION['date_quiz']=strtotime($user['date_quiz']);
+        
+
+        ?>
