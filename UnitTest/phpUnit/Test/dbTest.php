33fd2a97a00748b8a7a4b818e7e09ad4bbd3a9f7
mreouven <32807666+mreouven@users.noreply.github.com>
2018-01-07 11:58:28 +0200

    x

--- UnitTest/phpUnit/Test/dbTest.php
+++ UnitTest/phpUnit/Test/dbTest.php
@@ -6,24 +6,26 @@ class DbTest extends \PHPUnit_Framework_TestCase
 {
 	public function testhost()
 		{
-			$foo = $host;
-		    $this->assertEquals($foo,'mreouven.y.me:3307');
+		    $this->assertTrue(!isset($host));
 		}
 	public function testuser()
 		{
-			$foo2 = $user;
-		    $this->assertEquals($foo2,'root');
+		
+		    $this->assertTrue(!isset($user));
 		}
 	public function testpass()
 		{
-			$foo3 = $pass;
-		    $this->assertEquals($foo3,'reen');
+			$this->assertTrue(!isset($pass));
 		}
 	public function testdb()
 		{
-			$foo4 = $db;
-			$this->assertEquals($foo4,'ts');
+		
+			$this->assertTrue(!isset($db));
 		}	
 		
-    //
+    public function testConnection()
+		{
+		
+			$this->assertTrue(!isset($mysqli));
+		}
 }
