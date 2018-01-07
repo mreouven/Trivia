897f63cdec61d79f9c6f7cf3fe016f3bd0e768a4
mreouven <32807666+mreouven@users.noreply.github.com>
2018-01-07 11:52:45 +0200

    x

--- UnitTest/phpUnit/Test/QuestionTest.php
+++ UnitTest/phpUnit/Test/QuestionTest.php
@@ -5,7 +5,7 @@ include 'sessioncharge.php';
 $first_name = $_SESSION['first_name'];
 $last_name = $_SESSION['last_name'];
 $email = $_SESSION['email'];
-   $active = $_SESSION['active'];
+  $active = $_SESSION['active'];
 $level_user = $_SESSION['level_user'];
 
 class QuestionTest extends \PHPUnit_Framework_TestCase
@@ -35,20 +35,10 @@ class QuestionTest extends \PHPUnit_Framework_TestCase
 		    $level_user = $_SESSION['level_user'];
 		    $this->assertEquals( $level_user, $_SESSION['level_user']);
 		}
-	public function testLoggedIn()
-		{
-			( $_SESSION['logged_in'])!=true ;
-			$this->assertTrue($_SESSION['logged_in']);
-		}
-	public function testAnswerIsEquals()
-		{
-		    $reponsearr[$quesc]=$donnees['reponse'];
-		    $this->assertEquals( $reponsearr[$quesc], $donnees['reponse']);
-		}
+
 	public function testDonneesIsEquals()
 		{
-			$donnees = $reponse->fetch();
-			$this->assertEquals($donnees, $reponse->fetch());
+			$this->assertTrue(!isset($user));
 		}
 
     //
--- UnitTest/phpUnit/Test/dbTest.php
+++ UnitTest/phpUnit/Test/dbTest.php
@@ -7,7 +7,7 @@ class DbTest extends \PHPUnit_Framework_TestCase
 	public function testhost()
 		{
 			$foo = $host;
-		    $this->assertEquals($foo,'mreouven.synology.me:3307');
+		    $this->assertEquals($foo,'mreouven.y.me:3307');
 		}
 	public function testuser()
 		{
@@ -17,12 +17,12 @@ class DbTest extends \PHPUnit_Framework_TestCase
 	public function testpass()
 		{
 			$foo3 = $pass;
-		    $this->assertEquals($foo3,'reouven');
+		    $this->assertEquals($foo3,'reen');
 		}
 	public function testdb()
 		{
 			$foo4 = $db;
-			$this->assertEquals($foo4,'accounts');
+			$this->assertEquals($foo4,'ts');
 		}	
 		
     //
